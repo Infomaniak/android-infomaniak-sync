ac03831dc32229a4ccbf2a8b047e36a88ed4f212
Joris Bodin <joris.bodin@infomaniak.ch>
2019-01-16 16:07:24 +0100

    1.1.1 Multi AddressBook possible

--- app/src/main/java/at/bitfire/davdroid/resource/LocalAddressBook.kt
+++ app/src/main/java/at/bitfire/davdroid/resource/LocalAddressBook.kt
@@ -18,11 +18,14 @@ import android.provider.ContactsContract
 import android.provider.ContactsContract.CommonDataKinds.GroupMembership
 import android.provider.ContactsContract.Groups
 import android.provider.ContactsContract.RawContacts
+import android.util.Base64
+import at.bitfire.davdroid.DavUtils
 import at.bitfire.davdroid.R
 import at.bitfire.davdroid.log.Logger
 import at.bitfire.davdroid.model.CollectionInfo
 import at.bitfire.davdroid.model.SyncState
 import at.bitfire.vcard4android.*
+import java.io.ByteArrayOutputStream
 import java.util.*
 import java.util.logging.Level
 
@@ -71,7 +74,18 @@ class LocalAddressBook(
                 .toList()
 
         fun accountName(mainAccount: Account, info: CollectionInfo): String {
-            return mainAccount.name.replace("'"," ")
+            val baos = ByteArrayOutputStream()
+            baos.write(info.url.hashCode())
+            val hash = Base64.encodeToString(baos.toByteArray(), Base64.NO_WRAP or Base64.NO_PADDING)
+
+            val sb = StringBuilder(mainAccount.name.let {
+                if (it.isNullOrEmpty())
+                    DavUtils.lastSegmentOfUrl(info.url)
+                else
+                    it
+            })
+            sb.append(" (${info.displayName} $hash)")
+            return sb.toString().replace("'"," ")
         }
 
         fun initialUserData(mainAccount: Account, url: String): Bundle {
